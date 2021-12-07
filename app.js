4db31cab9f4e460e3534950b971d325cfd4b9e81
maaz619 <maazahmedlfc@gmail.com>
2021-12-08 01:36:19 +0530

    Added everything without styling

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-var a = "hello";
+var btnHandler = document.querySelector(".btn");
+
+var createDiv = document.querySelector(".output-text");
+
+var textInput = document.querySelector("#input-area");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json?text=";
+
+function getTranslatedURL(text) {
+  return serverURL + text;
+}
+function errorHandler(error) {
+  return alert("Something went wrong");
+}
+function clickHandler() {
+  var input = textInput.value;
+
+  fetch(getTranslatedURL(input))
+    .then(async (response) => await response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      createDiv.innerHTML = translatedText;
+    })
+    .catch(errorHandler);
+}
+btnHandler.addEventListener("click", clickHandler);
